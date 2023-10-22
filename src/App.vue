27379380abd0809b30b80669cae5ac675d21ab1c
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-10-22 18:07:00 +0300

    added search component to main page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper-hero">
+  <div class="wrapper-color__accent">
     <div class="container-mini">
       <Header />
     </div>
@@ -7,37 +7,52 @@
       <Hero />
     </div>
   </div>
-  <div class="wrapper-currently">
+  <div class="wrapper-color__dark">
     <div class="container-large">
       <WhatsOn />
     </div>
   </div>
+  <div class="wrapper-color__light">
+    <div class="container-midi">
+      <SearchInput />
+
+    </div>
+  </div>
+
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
 import WhatsOn from "./components/layout/WhatsOn.vue";
+import SearchInput from './components/Search/SearchInput.vue'
 
 export default {
   components: {
     Header,
     Hero,
     WhatsOn,
+    SearchInput
   },
 };
 </script>
 
 <style scoped>
-.wrapper-hero {
+.wrapper-color__accent {
  background-color: var(--color-background-yellow);
   padding-bottom: 2px;
 }
 
-.wrapper-currently {
-  
+.wrapper-color__dark {
   background-color: var(--color-background-black);
   padding-bottom: 6px;
+}
+
+.wrapper-color__light {
+  background-color: var(--color-background-white);
+  padding-bottom: 6px;
+  
+
 }
 </style>
 
---
+++ src/components/Search/SearchInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="search-container">
+    <input type="text" class="search-input" placeholder="Search..." />
+    <button class="search-btn">
+      <img class="" src="../../assets/svg/icons8-search.svg" alt="" />
+    </button>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.search-container {
+  padding: 70px 0;
+  position: relative;
+}
+
+.search-input {
+  width: 100%;
+  height: 60px;
+  padding: 15px 25px;
+
+  border: 1px solid var(--color-background-black);
+  border-radius: 30px;
+  color: var(--font-color-body-black);
+  font-size: 17px;
+  font-family: var(--font-family-body);
+  letter-spacing: 1.3px;
+}
+
+.search-input:focus {
+  outline: none;
+  box-shadow: 0 0 5px #4f4f4f;
+}
+
+.search-input::placeholder {
+  color: var(--font-color-body-black);
+  opacity: 1;
+  font-size: 17px;
+  font-family: var(--font-family-body);
+}
+
+.search-btn {
+  position: absolute;
+  right: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.search-btn img {
+  width: 30px;
+}
+
+.search-btn:active {
+  transform: translateY(-48%);
+}
+</style>
