8e54c26a00236a2a0719bea5e323a78aa314305a
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-10-22 12:44:07 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+
+
+<template>
+  <div class="hero">
+  <div class="container-mini">
+    <Header />
+    </div>
+    
+      
+
+   
+   
+
+  </div>
+  
+   
+
+
+  
+  
+
+</template>
+
+
+
+<script>
+
+import Header from './components/Header.vue'
+
+
+export default {
+  components: {
+    Header
+  }
+
+ 
+
+}
+
+
+
+
+
+</script>
+
+<style scoped>
+.hero {
+  background-color: #fce373;
+  height: 400px;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-list">
+      <li class="navbar-item"><a href="#">Collections</a></li>
+      <li class="navbar-item"><a href="#">Exibition</a></li>
+      <li class="navbar-item logo">
+        <a href="#">
+          <span>HARVARD ART</span>
+          <span class="logo-museum">MUSEUM</span>
+        </a>
+      </li>
+      <li class="navbar-item"><a href="#">Events</a></li>
+      <li class="navbar-item"><a href="#">Publications</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script></script>
+
+<style scoped>
+.navbar {
+  padding-top: 30px;
+}
+
+.logo-container {
+  font-family: var(--font-family-accent);
+}
+
+.navbar-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+}
+
+.logo a {
+  text-align: center; 
+  display: block; 
+}
+
+.logo span {
+  display: block; 
+}
+
+.logo-museum {
+    transform: rotate(180deg);
+}
+
+.logo {
+font-family: var(--font-family-accent);
+line-height: 29px;
+font-size: 30px;
+}
+
+
+</style>
