66d31798098e86e2c8a59cc89fe0c57c37c4f39c
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-10-23 20:38:29 +0300

    updated class names according to BEM

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,71 +1,71 @@
 <template>
-  <div class="wrapper-color__accent">
+  <div class="wrapper-color--accent">
     <div class="container-mini">
       <Header />
     </div>
     <div class="container-midi">
       <div class="hero">
-        <h1 class="header">
+        <h1 class="hero__header">
           ONE PLACE <br />
           THREE MUSEUMS
         </h1>
-        <h3 class="subheader">
+        <h3 class="hero__subheader">
           the Fogg, Busch-Reisinger, and Arthur M. Sackler Museums
         </h3>
       </div>
     </div>
   </div>
 
-  <div class="wrapper-color__dark">
+  <div class="wrapper-color--dark">
     <div class="container-large">
       <div class="currently">
-        <h2 class="subheader-currently">WHAT'S ON</h2>
+        <h2 class="currently__subheader">WHAT'S ON</h2>
         <Slider />
 
         <a class="btn btn-event-link" href="#">View all events</a>
       </div>
     </div>
   </div>
-  <div class="wrapper-color__light">
+  <div class="wrapper-color--light">
     <div class="container-midi">
       <SearchInput />
     </div>
   </div>
 
-  <div class="wrapper-color__dark">
+  <div class="wrapper-color--dark">
     <div class="home-section">
-      <div class="left-content">
-        <img class="main-img" src="../assets//Museum-img.png" alt="museum" />
+      <div class="home-content__left-content">
+        <img class="home-section__main-img" src="../assets//Museum-img.png" alt="Museum" />
       </div>
 
-      <div class="right-content">
-        <div class="top-content">
-          <h2 class="subheader">PLAN YOUR VISIT</h2>
-          <p class="work-time">
+      <div class="home-section__right-content">
+        <div class="home-section__top-content">
+          <h2 class="home-section__subheader">PLAN YOUR VISIT</h2>
+          <p class="home-section__work-time">
             Open Tuesday-Sunday 10am-5pm <br />
             <span class="work-time__highlights">Closed on major holidays</span>
           </p>
-          <p class="tickets-info">
-            <span class="tickets-info__highlights">Tickets</span> <br />
+          <p class="home-section__tickets-info">
+            <span class="home-section__tickets-info--highlights">Tickets</span> <br />
             The museums are free to all visitors. Please check in at the
             Admissions desk. <br />
             <br />
-            <span class="ticket-info__small"
+            <span class="home-section__ticket-info--small"
               >This new initiative is made possible by a generous contribution
               from the Estate of David Rockefeller and support from the Office
               of the President at Harvard University.</span
             >
           </p>
-          <a class="more-details" href="#">More details</a>
+          <a class="home-section__more-details" href="#">More details</a>
         </div>
 
-        <div class="bottom-content">
-          <h2 class="subheader">LOCATION</h2>
-          <div class="location-container">
-            <div class="museum-icon">
+        <div class="home-section__bottom-content">
+          <h2 class="home-section__subheader">LOCATION</h2>
+          <div class="home-section__location-container">
+            <div class="home-section__museum-icon">
               <img src="../assets/svg/icon-museum.png" alt="Museum Icon" />
             </div>
-            <div class="museum-address">
+            <div class="home-section__museum-address">
               Harvard Art Museums <br />
               32 Quincy Street <br />
               Cambridge, MA 02138 <br />
@@ -77,7 +77,7 @@
     </div>
   </div>
 
-  <div class="wrapper-color__white">
+  <div class="wrapper-color--white">
     <div class="container-midi">
       <Footer />
     </div>
@@ -86,7 +86,6 @@
 
 <script>
 import Header from "../components/layout/Header.vue";
-
 import SearchInput from "../components/Search/SearchInput.vue";
 import Footer from "../components/layout/Footer.vue";
 import Slider from "../components/Slider/Slider.vue";
@@ -104,27 +103,27 @@ export default {
 </script>
 
 <style scoped>
-.wrapper-color__accent {
+.wrapper-color--accent {
   background-color: var(--color-background-yellow);
   padding-bottom: 2px;
 }
 
-.wrapper-color__dark {
+.wrapper-color--dark {
   background-color: var(--color-background-black);
   padding-bottom: 6px;
 }
 
-.wrapper-color__light {
+.wrapper-color--light {
   background-color: var(--color-background-white);
   padding-bottom: 6px;
 }
 
-.header {
+.hero__header {
   text-align: center;
   margin-top: 70px;
 }
 
-.subheader {
+.hero__subheader {
   text-align: center;
   margin-top: 35px;
   line-height: 35px;
@@ -136,7 +135,7 @@ export default {
   align-items: center;
   color: var(--font-color-body-white);
 }
-.subheader-currently {
+.currently__subheader{
   text-align: center;
   padding-top: 100px;
   margin-bottom: 50px;
@@ -150,7 +149,7 @@ export default {
   background-color: var(--color-background-yellow);
 }
 
-.left-content {
+.home-content__left-content {
   flex: 1;
 }
 
@@ -158,33 +157,33 @@ export default {
   display: flex;
 }
 
-.right-content {
+.home-section__right-content {
   flex: 2;
   display: flex;
   flex-direction: column;
 }
 
-.main-img {
+.home-section__main-img {
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
 
-.work-time__highlights {
+.home-section__work-time--highlights {
   text-decoration: underline;
   font-weight: bold;
 }
 
-.tickets-info__highlights {
+.home-section__tickets-info--highlights {
   text-decoration: underline;
 }
 
-.ticket-info__small {
+.home-section__ticket-info--small {
   font-size: 18px;
   color: var(--font-color-body-grey);
 }
 
-.location-container {
+.home-section__location-container {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -192,7 +191,7 @@ export default {
   margin-top: 50px;
 }
 
-.top-content {
+.home-section__top-content {
   color: var(--font-color-body-black);
   padding: 40px 120px;
   font-size: 23px;
@@ -201,17 +200,17 @@ export default {
   align-items: center;
 }
 
-.work-time {
+.home-section__work-time {
   margin-top: 30px;
   text-align: center;
 }
 
-.tickets-info {
+.home-section__tickets-info {
   margin-top: 60px;
   text-align: center;
 }
 
-.top-content {
+.home-section__top-content {
   color: var(--font-color-body-black);
   padding: 40px 120px;
   font-size: 23px;
@@ -219,20 +218,20 @@ export default {
   background-color: var(--color-background-yellow);
 }
 
-.more-details {
+.home-section__more-details {
   margin-top: 50px;
   font-size: 18px;
   text-decoration: underline;
 }
 
-.museum-icon {
+.home-section__museum-icon {
   width: 80px;
 }
 
-.subheader {
+.home-section__subheader {
   text-align: center;
 }
-.location-container {
+.home-section__location-container {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -240,7 +239,7 @@ export default {
   margin-top: 50px;
 }
 
-.bottom-content {
+.home-section__bottom-content {
   flex: 1;
   background-color: var(--color-background-black);
   color: var(--font-color-body-white);
