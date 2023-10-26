0d6893cb61ce7dd9c9299c170952a7c8ac4d7d12
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-10-26 10:54:01 +0300

    changed class names in Card.vue according to BEM

--- src/components/Slider/Card.vue
+++ src/components/Slider/Card.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card">
-    <div class="card__img-container">
-      <img class="card__img" :src="imageSrc" :alt="imageAlt" />
-      <div class="card__date">{{ date }}</div>
+    <div class="card-img-container">
+      <img class="card-img-container__image" :src="imageSrc" :alt="imageAlt" />
+      <div class="card-img-container__date">{{ date }}</div>
     </div>
     <div class="card__title">
       <a :href="linkHref">{{ title }}</a>
@@ -38,7 +38,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 // Fonts
 $font-family-body: "Didact Gothic", Arial, Helvetica, sans-serif;
 $font-family-accent: "Futura", Helvetica, Arial, sans-serif;
@@ -47,35 +46,33 @@ $font-family-accent: "Futura", Helvetica, Arial, sans-serif;
 $font-color-body-black: #231f20;
 $font-color-body-white: #e1e0e0;
 $font-color-body-grey: rgb(74, 71, 71);
-$color-background-yellow: #fce373;;
+$color-background-yellow: #fce373;
 $color-background-black: #151515;
 $color-background-white: #faf9f7;
 
-
 .card {
-    max-width: 500px;
-  
-    &__img-container {
-      position: relative;
+  max-width: 500px;
+
+  &-img-container {
+    position: relative;
+
+    &__date {
+      position: absolute;
+      bottom: 20px;
+      left: 10px;
+      color: $font-color-body-grey;
+      background-color: #dfdfdf;
+      padding: 2px 10px;
     }
-  
-      &__date {
-        position: absolute;
-        bottom: 20px;
-        left: 10px;
-        color: $font-color-body-grey;
-        background-color: #dfdfdf;
-        padding: 2px 10px;
-      }
-  
-      &__img {
-        width: 500px;
-        height: 400px;
-      }
-    
-  
-    &__title {
-      font-family: $font-family-accent;
+
+    &__image {
+      width: 500px;
+      height: 400px;
     }
   }
+
+  &__title {
+    font-family: $font-family-accent;
+  }
+}
 </style>
