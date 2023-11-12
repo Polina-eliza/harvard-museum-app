877588cf2756956de9d7a9af22b953ca31a35f91
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-11-12 16:05:59 +0200

    added v-for for dropdown list

--- src/components/Dropdown/FilterDropdown.vue
+++ src/components/Dropdown/FilterDropdown.vue
@@ -1,56 +1,38 @@
 <template>
   <div class="collections-header__dropdown">
-    <div class="collections-header__dropdown-item">
-      <button type="button" class="collections-header__dropdown-toggle">
-        Classification
-      </button>
+    <div v-for="(item, index) in dropdownItems" :key="index" class="collections-header__dropdown-item">
+      <div type="button" class="collections-header__dropdown-toggle">
+        {{ item.title }}
+      </div>
       <ul class="collections-header__dropdown-list">
-        <li class="collections-header__dropdown-checkbox">
-          <input type="checkbox" name="classification" />
-          <label for="accessories">Accessories</label>
-        </li>
-        <li class="collections-header__dropdown-checkbox">
-          <input type="checkbox" name="classification" />
-          <label for="accessories">Accessories</label>
-        </li>
-        <li class="collections-header__dropdown-checkbox">
-          <input type="checkbox" name="classification" />
-          <label for="accessories">Accessories</label>
-        </li>
-      </ul>
-    </div>
-    <div class="collections-header__dropdown-item">
-      <button class="collections-header__dropdown-toggle">
-        Department <span>&raquo;</span>
-      </button>
-      <ul class="collections-header__dropdown-list">
-        <li class="collections-header__dropdown-checkbox">
-          <input type="checkbox" name="classification" />
-          <label for="accessories">Accessories</label>
-        </li>
-      </ul>
-    </div>
-    <div class="collections-header__dropdown-item">
-      <button class="collections-header__dropdown-toggle">
-        Culture <span>&raquo;</span>
-      </button>
-      <ul class="collections-header__dropdown-list">
-        <li class="collections-header__dropdown-checkbox">
-          <input type="checkbox" name="classification" />
-          <label for="accessories">Accessories</label>
-        </li>
-      </ul>
-    </div>
-    <div class="collections-header__dropdown-item">
-      <button class="collections-header__dropdown-toggle">
-        Technique <span>&raquo;</span>
-      </button>
-      <ul class="collections-header__dropdown-list">
-        <li class="collections-header__dropdown-checkbox">
-          <input type="checkbox" name="classification" />
-          <label for="accessories">Accessories</label>
+        <li v-for="(option, index) in item.options" :key="index" class="collections-header__dropdown-checkbox">
+          <input type="checkbox" :name="item.name" />
+          <label :for="type">{{ type }}</label>
         </li>
       </ul>
     </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      dropdownItems: [
+        {
+          title: 'Classification',
+          name: 'classification',
+          type: ['']
+        },
+        {
+          title: 'Department',
+          name: 'department',
+          type: ['']
+        },
+
+      ]
+    };
+  }
+};
+</script>
