67adeea4ff485db2770f6207a3f70c9c9be2f690
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-11-16 15:35:59 +0200

    modified collectionsService to class

--- src/service/collections/collectionsService.js
+++ src/service/collections/collectionsService.js
@@ -1,21 +1,29 @@
 import ArtworkApi from "../../api/collections/collectionsApi";
 import DefaultImg from "../../assets/17816812.jpeg";
 
-export default {
+class ArtworkService {
+  constructor() {
+    this.artworkApi = new ArtworkApi();
+  }
+
   async getArtworksForCollections(amount, page) {
     const { data } = await ArtworkApi.getArtworksByPage(amount, page);
     return data.filter((artwork) => artwork.images && "web" in artwork.images);
-  },
+  }
+
   async searchArtworks(query, amount, page) {
     const { data } = await ArtworkApi.searchArtworksByQuery(query, amount, page);
     return data.filter((artwork) => artwork.images && "web" in artwork.images);
-  },
+  }
+
   getImageUrl(images) {
     if (images && images.web && images.web.url) {
       return images.web.url;
     } else {
       return DefaultImg;
     }
-  },
-};
+  }
+}
 
+
+export default new ArtworkService();
