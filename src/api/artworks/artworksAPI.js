34e25793cbc69bb8c124a041c05eb5bd9bc7aa15
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-11-17 09:56:59 +0200

    modified artworksApi file title

--- src/api/artworks/artworksAPI.js
+++ src/api/artworks/artworksAPI.js
@@ -1,10 +1,10 @@
-class ArtworksAPI {
+class ArtworksApi {
   static #baseURL = "https://openaccess-api.clevelandart.org";
 
   async getArtworks(limit = 10) {
     try {
       const response = await fetch(
-        `${ArtworksAPI.#baseURL}/api/artworks/?limit=${limit}`
+        `${ArtworksApi.#baseURL}/api/artworks/?limit=${limit}`
       );
       const { data } = await response.json();
       return data;
@@ -16,6 +16,6 @@ class ArtworksAPI {
   }
 }
 
-export default ArtworksAPI;
+export default ArtworksApi;
 
 
