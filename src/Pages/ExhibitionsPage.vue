60854ab45130d71e11084e0841681ca68649e34e
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-12-13 15:20:43 +0200

    created exhibitionsCurrent as a separate component

--- src/Pages/ExhibitionsPage.vue
+++ src/Pages/ExhibitionsPage.vue
@@ -10,26 +10,8 @@
       :key="index"
       class="exhibitions-current"
     >
-      <img
-        class="exhibitions-current__image"
-        :src="exhibition.imageURL"
-        :alt="exhibition.title"
-      />
-      <div class="exhibitions-current__information">
-        <div class="exhibitions-current__information-subheading">Current</div>
-        <div class="exhibitions-current__information-heading">
-          {{ exhibition.title }}
-        </div>
-        <div class="exhibitions-current__information-location">
-          {{ exhibition.organizer }}
-        </div>
-        <div class="exhibitions-current__information-opening-date">
-          {{ exhibition.opening_date }}
-        </div>
-        <div class="exhibitions-current__information-closing-date">
-          {{ exhibition.closing_date }}
-        </div>
-      </div>
+      <ExibitionCurrent :exhibition="exhibition"/>
+
     </div>
     <hr class="exhibitions-current__divider" />
   </div>
@@ -37,8 +19,12 @@
 
 <script>
 import exhibitionsService from "../service/exhibitions/exhibitionsService";
+import ExibitionCurrent from "../components/Exibitions/ExibitionCurrent.vue"
 
 export default {
+  components: {
+    ExibitionCurrent
+  },
   data() {
     return {
       exhibitions: [],
