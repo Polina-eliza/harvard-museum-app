4467d7fb60191f0bea582688ec61b18a73d7feaf
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-12-13 19:47:46 +0200

    issue with process is not defined

--- src/main.js
+++ src/main.js
@@ -5,33 +5,14 @@ import router from './router';
 import Toaster from '@meforma/vue-toaster';
 import { createVuetify } from 'vuetify';
 import store from './store'; 
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAqaTTB6uJCt5Imlz6MyumnH2KY6OfIQOI",
-  authDomain: "museum-project-vue.firebaseapp.com",
-  projectId: "museum-project-vue",
-  storageBucket: "museum-project-vue.appspot.com",
-  messagingSenderId: "845984490847",
-  appId: "1:845984490847:web:81596be909b8993f3e82ea"
-};
-
-initializeApp(firebaseConfig);
-
+import './firebaseInit'; 
 
 
 
 const vuetify = createVuetify()
-
 const app = createApp(App);
 
 app.use(store);
-
 app.use(router);
 app.use(Toaster);
 app.use(vuetify);
