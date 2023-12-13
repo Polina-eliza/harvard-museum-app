df4138e2b502ea7b3c8c4a4f388d9332532c5912
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-12-13 14:45:25 +0200

    deleted vuetify dependency

--- src/components/Form/LoginForm.vue
+++ src/components/Form/LoginForm.vue
@@ -43,7 +43,7 @@
 
 import { validateLoginForm, loginUserWithEmailAndPassword } from "../../service/form/loginService";
 import { useRouter } from "vue-router";
-import { useToast } from "@meforma/vue-toaster"; 
+import { createToaster } from "@meforma/vue-toaster";
 
 export default {
   data() {
@@ -57,15 +57,19 @@ export default {
     };
   },
   setup() {
+    const toaster = createToaster();
     const router = useRouter();
-    const toast = useToast();
+<<<<<<< HEAD
+    const toaster = createToaster();
+=======
+>>>>>>> 8ce88043382773e001b4ac11434bdaf6afee6651
 
     const loginFormSubmit = () => {
       const errors = validateLoginForm(this.email, this.password);
       if (!errors.email && !errors.password) {
         loginUserWithEmailAndPassword(this.email, this.password)
           .then(() => {
-            toast.success("Login successful");
+            toaster.success("Login successful");
             router.push("/collections");
           })
           .catch((error) => {
@@ -83,7 +87,7 @@ export default {
               default:
                 errorMessage = "Email or password was incorrect";
             }
-            toast.error(errorMessage);
+            toaster.error(errorMessage);
             this.email = "";
             this.password = "";
           });
@@ -92,7 +96,7 @@ export default {
       }
     };
 
-    return { router, loginFormSubmit, toast };
+    return { router, loginFormSubmit, toaster };
   },
 };
 </script>
