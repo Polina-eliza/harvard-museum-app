f49ccdf2be023622d5afb45d8ae35698a5d9ccd1
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-12-20 15:05:20 +0200

    created exhibitionImagesStorage for img

--- src/service/exhibitions/exhibitionsService.js
+++ src/service/exhibitions/exhibitionsService.js
@@ -1,5 +1,6 @@
 import ExhibitionsApi from "../../api/exhibitions/exhibitionsApi";
 import DefaultImg from "../../assets/17816812.jpeg";
+import { exhibitionImagesStorage } from "../../exhibitionImagesStorage";
 
 class ExhibitionsService {
   constructor() {
@@ -11,12 +12,18 @@ class ExhibitionsService {
     try {
       const data = await this.api.fetchExhibitions(searchParams); 
       return data.data.map((exhibition) => {
-        const imageURL = this.getImageUrl(exhibition.artworks?.[0]?.images);
+        const localImage = this.getLocalImageForExhibition(exhibition.id);
+        const imageURL = localImage || this.getImageUrl(exhibition.artworks?.[0]?.images);
         return { ...exhibition, imageURL };
       });
     } catch (error) {
       throw new Error("Error fetching exhibitions: " + error.message);
     }
+  }
+
+  getLocalImageForExhibition(exhibitionId) {
+    const localImageObj = exhibitionImagesStorage.find(image => image.id === exhibitionId);
+    return localImageObj ? localImageObj.imageURL : null;
   }
 
   getImageUrl(images) {
