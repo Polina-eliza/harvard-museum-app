ff224226981c4d271649a8ee6bc9f803b4695c02
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-12-24 22:21:24 +0200

    8_testing_all_js_files: created test for authStateService.js

---
+++ src/service/common/authStateService.test.js
@@ -0,0 +1,37 @@
+import { getAuth, signOut } from "firebase/auth";
+import { handleSignOut } from './authStateService';
+
+jest.mock('firebase/auth');
+
+describe('handleSignOut', () => {
+  const mockStore = { commit: jest.fn() };
+  const mockRouter = { push: jest.fn() };
+  const mockToaster = {
+    success: jest.fn(),
+    error: jest.fn()
+  };
+
+  global.localStorage = {
+    removeItem: jest.fn()
+  };
+
+  test('successfully signs out', async () => {
+    signOut.mockResolvedValue(); 
+
+    await handleSignOut(mockStore, mockRouter, mockToaster);
+
+    expect(localStorage.removeItem).toHaveBeenCalledWith("isLoggedIn");
+    expect(mockStore.commit).toHaveBeenCalledWith("setLoginStatus", false);
+    expect(mockRouter.push).toHaveBeenCalledWith("/home");
+    expect(mockToaster.success).toHaveBeenCalledWith("Successfully logged out");
+  });
+
+  test('handles sign out failure', async () => {
+    const mockError = new Error('Sign out failed');
+    signOut.mockRejectedValue(mockError); 
+
+    await handleSignOut(mockStore, mockRouter, mockToaster);
+
+    expect(mockToaster.error).toHaveBeenCalledWith(`Sign out error: ${mockError}`);
+  });
+});
