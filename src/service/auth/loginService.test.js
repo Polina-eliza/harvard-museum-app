3c30420e154f55cace2a8af6b83f2f9fcd3ab960
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-12-24 12:28:19 +0200

    8_testing_all_js_files

--- src/service/auth/loginService.test.js
+++ src/service/auth/loginService.test.js
@@ -1,29 +1,26 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import store from "../../store";
-import { loginUserWithEmailAndPassword } from "./loginService";
+import { loginUserWithEmailAndPassword, validateLoginForm } from "./loginService"; 
 
 jest.mock('firebase/auth');
 jest.mock('../../store');
 
-describe('loginUserWithEmailAndPassword', () => {
+global.localStorage = { setItem: jest.fn() };
+
+describe('loginService', () => {
+
+  describe('loginUserWithEmailAndPassword', () => {
 
     test('should set login status on successful login', async () => {
-      global.localStorage = { setItem: jest.fn() };
-  
-    
       signInWithEmailAndPassword.mockResolvedValue({});
-  
      
       await loginUserWithEmailAndPassword('test@example.com', 'password1234567');
   
-      
       expect(localStorage.setItem).toHaveBeenCalledWith('isLoggedIn', 'true');
       expect(store.commit).toHaveBeenCalledWith('setLoginStatus', true);
     });
   
     test('should handle login failure', async () => {
-      global.localStorage = { setItem: jest.fn() };
-  
       signInWithEmailAndPassword.mockRejectedValue(new Error('Login failed'));
   
       await expect(loginUserWithEmailAndPassword('test@example.com', 'wrongpassword'))
@@ -33,3 +30,30 @@ describe('loginUserWithEmailAndPassword', () => {
       expect(store.commit).not.toHaveBeenCalled();
     });
   });
+
+  describe('validateLoginForm', () => {
+    test('returns no errors for valid input', () => {
+      const errors = validateLoginForm('test@example.com', 'password1234567');
+      expect(errors).toEqual({ email: null, password: null });
+    });
+
+    test('returns an error for invalid email format', () => {
+      const errors = validateLoginForm('invalidemail', 'password1234567');
+      expect(errors.email).toBe('Invalid email format');
+      expect(errors.password).toBeNull();
+    });
+
+    test('returns an error for short password', () => {
+      const errors = validateLoginForm('test@example.com', 'pass');
+      expect(errors.password).toBe('Password should be at least 6 characters');
+      expect(errors.email).toBeNull();
+    });
+
+    test('returns errors for both invalid email and short password', () => {
+      const errors = validateLoginForm('invalidemail', 'pass');
+      expect(errors.email).toBe('Invalid email format');
+      expect(errors.password).toBe('Password should be at least 6 characters');
+    });
+  });
+  
+});
