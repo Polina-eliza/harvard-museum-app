4b802c8f6811f3e5592cbd006155a6320c59931a
Polina <118730269+Polina-eliza@users.noreply.github.com>
2024-01-08 13:21:31 +0200

    9_add_vue_tests: created test for FavoritesPage

---
+++ src/Pages/FavoritesPage.test.js
@@ -0,0 +1,17 @@
+import { shallowMount } from "@vue/test-utils";
+import FavoritesPage from "./FavoritesPage.vue";
+import Favorites from "../components/Favorites/Favorites.vue"
+
+
+describe('FavoritesPage', () => {
+    test('contains Favorites', () => {
+        const wrapper = shallowMount(FavoritesPage);
+        expect (wrapper.findComponent(Favorites).exists()).toBe(true);
+    })
+
+    test('has the right structure', () => {
+        const wrapper = shallowMount(FavoritesPage);
+        expect(wrapper.find('.favorites-saved').exists()).toBe(true);
+        expect(wrapper.find('.favorites-saved__header').text()).toBe('npm Saved Collections')
+    })
+})
